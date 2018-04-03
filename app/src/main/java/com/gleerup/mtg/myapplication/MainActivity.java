9f74f33ca2affc0240ab5446bf633ed7257daa6a
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2018-04-03 18:24:32 +0200

    Added buttons and activities

--- app/src/main/java/com/gleerup/mtg/myapplication/MainActivity.java
+++ app/src/main/java/com/gleerup/mtg/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gleerup.mtg.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pushIncomeButton(View view){
+        Intent intent = new Intent(this, EnterIncome.class);
+        startActivity(intent);
+    }
+
+    public void pushExpenseButton(View view){
+        Intent intent = new Intent(this, EnterExpense.class);
+        startActivity(intent);
     }
 }
